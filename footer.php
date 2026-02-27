f630cfac35c5ceb8c9deb38863709e26e86591fd
ahsanjaved2002 <ahsanjaved2002@gmail.com>
2026-02-28 01:40:11 +0500

    Adding files

---
+++ footer.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+  <style>
+    #footer {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 75px;
+      background-color: #000000;
+      color: white;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <footer id="footer">
+    <div>
+      <b>COPYRIGHT © 2020<br>
+      Blood Bank & Donation Management<br>
+      ALL RIGHTS RESERVED.</b>
+    </div>
+  </footer>
+</body>
+</html>
